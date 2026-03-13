95462432b02dae2c7874bb0d9f01929170a6ebd1
RMLNETHWORK <rmlnethwork@gmail.com>
2026-03-13 10:38:38 +0800

    beng

---
+++ index.js
@@ -0,0 +1,51 @@
+function q1(){
+    const q0t = document.getElementById("Q0t");
+    const q1t = document.getElementById("Q1t");
+    const q2t = document.getElementById("Q2t");
+    const q3t = document.getElementById("Q3t");
+    const q4t = document.getElementById("Q4t");
+    q0t.style.display = "none";
+    q2t.style.display = "none";
+    q3t.style.display = "none";
+    q4t.style.display = "none";
+    q1t.style.display = "block";
+}
+
+function q2(){
+    const q0t = document.getElementById("Q0t");
+    const q1t = document.getElementById("Q1t");
+    const q2t = document.getElementById("Q2t");
+    const q3t = document.getElementById("Q3t");
+    const q4t = document.getElementById("Q4t");
+    q0t.style.display = "none";
+    q1t.style.display = "none";
+    q3t.style.display = "none";
+    q4t.style.display = "none";
+    q2t.style.display = "block";
+}
+
+function q3(){
+    const q0t = document.getElementById("Q0t");
+    const q1t = document.getElementById("Q1t");
+    const q2t = document.getElementById("Q2t");
+    const q3t = document.getElementById("Q3t");
+    const q4t = document.getElementById("Q4t");
+    q0t.style.display = "none";
+    q1t.style.display = "none";
+    q2t.style.display = "none";
+    q4t.style.display = "none";
+    q3t.style.display = "block";
+}
+
+function q4(){
+    const q0t = document.getElementById("Q0t");
+    const q1t = document.getElementById("Q1t");
+    const q2t = document.getElementById("Q2t");
+    const q3t = document.getElementById("Q3t");
+    const q4t = document.getElementById("Q4t");
+    q0t.style.display = "none";
+    q1t.style.display = "none";
+    q2t.style.display = "none";
+    q3t.style.display = "none";
+    q4t.style.display = "block";
+}
---
+++ pages/Q3/Q3.js
@@ -0,0 +1,25 @@
+function submit(){
+    let score = 0;
+    const answers = document.querySelectorAll('input[type="radio"]:checked');
+    
+    answers.forEach(answer => {
+        score += parseInt(answer.value);
+    });
+
+    // Store the score in localStorage
+    localStorage.setItem("quizScore", score);
+    localStorage.setItem("quizName", "Q3");
+
+    // Redirect to the results page
+    window.location.href = "/scoring.html";
+}
+
+function getInterpretation(score) {
+    if (score == 0) return "Grabe ba.";
+    else if (score <= 5) return "Needs Improvement.";
+    else if (score <= 10) return "There is still room for improvement.";
+    else if (score <= 15) return "You're almost there. Keep it up!";
+    else if (score <= 19) return "You made it! You're a GENIUS!";
+    else if (score == 20) return "You are a master of Analytic Geometry! In this quiz I guess. ;)";
+    else return "";
+}
